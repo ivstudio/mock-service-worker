5f81c016b7bb81518e3b8d10e2307187a1684386
Iggy Villamar <iggy@ivstudio.com>
2024-08-21 18:29:47 -0400

    add spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <div className="size-8 animate-spin rounded-full border-4 border-gray-200 border-t-transparent" />
+        </div>
+    );
+};
+
+export default Spinner;
