44bb19eeae56429713052040dff3a1508662b00b
Iggy Villamar <iggy@ivstudio.com>
2024-08-22 16:41:19 -0400

    add global variable to conditionally run MSW and fix the static devServer directory

--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -6,18 +6,25 @@ import 'webpack-dev-server';
 
 import commonConfig from './webpack.common';
 
+const isMSW = process.env.MOCK_SERVICE_WORKER === 'true';
+
 const devConfig: webpack.Configuration = {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         static: {
-            directory: path.join(__dirname, '../dist'),
+            directory: path.join(__dirname, '../public'),
         },
         hot: true,
         open: true,
         port: 3000,
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()],
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.MOCK_SERVICE_WORKER': JSON.stringify(isMSW),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 };
 
 export default merge(commonConfig, devConfig);
