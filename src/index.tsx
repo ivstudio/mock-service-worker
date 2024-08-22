c3da937c79b9330134364c10ea72ddaad9894be1
Iggy Villamar <iggy@ivstudio.com>
2024-08-22 16:53:08 -0400

    defer application rendering until Service Worker registration is complete in development

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,20 @@ import '@/styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+(async function entry() {
+    /*
+        This ensures the mock service worker could only be enabled in development.
+        To enable MSW, run: npm run dev:mock
+        More details: https://mswjs.io/docs/integrations/browser
+    */
+    if (process.env.NODE_ENV === 'development') {
+        const { isMockServiceEnabled } = await import('./mocks/browser');
+        await isMockServiceEnabled();
+    }
+
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+})();
