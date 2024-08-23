843a44aa559fcff985c77ca326f013f58ad8bcf3
ivstudio <ivstudio@users.noreply.github.com>
2024-08-23 02:26:37 +0000

    deploy: 4237f116e5551a52c131133a73daa764b7a126c6

--- src/components/List/ListLabels.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {
-    children: React.ReactNode;
-}
-const ListLabels = ({ children }: Props) => {
-    return <ul className="flex gap-1 text-sm">{children}</ul>;
-};
-
-export default ListLabels;
--- src/components/List/ListRow.tsx
+++
@@ -1,9 +0,0 @@
-interface Props {
-    children: React.ReactNode;
-}
-
-const ListRow = ({ children }: Props) => {
-    return <div className="flex flex-col hover:text-blue-200">{children}</div>;
-};
-
-export default ListRow;
