20cce84948616d70b9542cb002c6dbbc9000bc77
Iggy Villamar <iggy@ivstudio.com>
2025-01-25 11:27:10 -0500

    fix: remove unused var

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,7 +3,6 @@ interface Props {
 }
 
 const List = ({ children }: Props) => {
-    const test = 'test';
     return (
         <section className="mx-auto flex max-w-3xl flex-col rounded-lg border border-gray-800 bg-gray-950">
             {children}
